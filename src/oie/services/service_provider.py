393c95554c9ccbd8cfe7e914991de9bd7f0f145c
Juan Jose Jair Huerta Morales <jjhuertapsic@gmail.com>
2026-06-29 15:25:47 -0600

    Move repository-backed service wiring into service provider

--- src/oie/services/service_provider.py
+++ src/oie/services/service_provider.py
@@ -5,8 +5,13 @@ from dataclasses import dataclass
 from oie.orchestration.run_context import RunContext
 from oie.persistence.context import PersistenceContext
 from oie.persistence.repository_provider import RepositoryProvider
+from oie.services.company_identity_service import CompanyIdentityService
 from oie.services.db_export_service import DBExportService
 from oie.services.executive_summary_service import ExecutiveSummaryService
+from oie.services.historical_intelligence_service import HistoricalIntelligenceService
+from oie.services.market_trends_service import MarketTrendsService
+from oie.services.master_dedup_service import MasterDedupService
+from oie.services.opportunity_dataset_service import OpportunityDatasetService
 from oie.services.outbound_export_service import OutboundExportService
 from oie.services.persistence_service import PersistenceService
 from oie.services.provider_control_service import ProviderControlService
@@ -24,6 +29,11 @@ class ServiceProvider:
     outbound_export_service: OutboundExportService
     executive_summary_service: ExecutiveSummaryService
     run_readiness_service: RunReadinessService
+    company_identity_service: CompanyIdentityService
+    master_dedup_service: MasterDedupService
+    opportunity_dataset_service: OpportunityDatasetService
+    historical_intelligence_service: HistoricalIntelligenceService
+    market_trends_service: MarketTrendsService
 
     @classmethod
     def from_run_context(cls, ctx: RunContext) -> "ServiceProvider":
@@ -39,4 +49,9 @@ class ServiceProvider:
             outbound_export_service=OutboundExportService(ctx, persistence=persistence_service.persistence),
             executive_summary_service=ExecutiveSummaryService(ctx, persistence=persistence_service.persistence),
             run_readiness_service=RunReadinessService(ctx, persistence=persistence_service.persistence),
+            company_identity_service=CompanyIdentityService(ctx, repositories=persistence_service.repositories),
+            master_dedup_service=MasterDedupService(ctx, repositories=persistence_service.repositories),
+            opportunity_dataset_service=OpportunityDatasetService(ctx, repositories=persistence_service.repositories),
+            historical_intelligence_service=HistoricalIntelligenceService(ctx, repositories=persistence_service.repositories),
+            market_trends_service=MarketTrendsService(ctx, repositories=persistence_service.repositories),
         )
