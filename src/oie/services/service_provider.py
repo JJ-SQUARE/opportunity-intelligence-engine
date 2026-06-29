d611dbfc31ea12e9147e1e82d3b5d840e7f61200
Juan Jose Jair Huerta Morales <jjhuertapsic@gmail.com>
2026-06-29 15:18:56 -0600

    Move persistence-backed service wiring into service provider

--- src/oie/services/service_provider.py
+++ src/oie/services/service_provider.py
@@ -5,8 +5,12 @@ from dataclasses import dataclass
 from oie.orchestration.run_context import RunContext
 from oie.persistence.context import PersistenceContext
 from oie.persistence.repository_provider import RepositoryProvider
+from oie.services.db_export_service import DBExportService
+from oie.services.executive_summary_service import ExecutiveSummaryService
+from oie.services.outbound_export_service import OutboundExportService
 from oie.services.persistence_service import PersistenceService
 from oie.services.provider_control_service import ProviderControlService
+from oie.services.run_readiness_service import RunReadinessService
 
 
 @dataclass(frozen=True)
@@ -16,6 +20,10 @@ class ServiceProvider:
     persistence: PersistenceContext
     repositories: RepositoryProvider
     provider_control_service: ProviderControlService
+    db_export_service: DBExportService
+    outbound_export_service: OutboundExportService
+    executive_summary_service: ExecutiveSummaryService
+    run_readiness_service: RunReadinessService
 
     @classmethod
     def from_run_context(cls, ctx: RunContext) -> "ServiceProvider":
@@ -27,4 +35,8 @@ class ServiceProvider:
             persistence=persistence_service.persistence,
             repositories=persistence_service.repositories,
             provider_control_service=provider_control_service,
+            db_export_service=DBExportService(ctx, persistence=persistence_service.persistence),
+            outbound_export_service=OutboundExportService(ctx, persistence=persistence_service.persistence),
+            executive_summary_service=ExecutiveSummaryService(ctx, persistence=persistence_service.persistence),
+            run_readiness_service=RunReadinessService(ctx, persistence=persistence_service.persistence),
         )
